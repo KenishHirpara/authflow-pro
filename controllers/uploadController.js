f3b61cdaf54bcb95a165ffc9b58fe68330fadac9
Kenish Hirpara <kenishhirpara2000@gmail.com>
2026-04-13 22:59:27 +0530

    Ready for deployment

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -2,23 +2,27 @@ const cloudinary = require("../config/cloudinary");
 
 exports.uploadImage = async (req, res) => {
   try {
+    // check file
     if (!req.file) {
       return res.status(400).json({ error: "No file uploaded" });
     }
 
+    // upload using buffer (IMPORTANT)
     const stream = cloudinary.uploader.upload_stream(
       { folder: "uploads" },
       (error, result) => {
         if (error) {
           return res.status(500).json({ error: error.message });
         }
-        res.json({
-          message: "Image uploaded",
+
+        return res.json({
+          message: "Image uploaded successfully",
           url: result.secure_url
         });
       }
     );
 
+    // send buffer to cloudinary
     stream.end(req.file.buffer);
 
   } catch (err) {
